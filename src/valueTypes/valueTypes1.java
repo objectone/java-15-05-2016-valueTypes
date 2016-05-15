0c01a57a322d0e59db1a24a647433a6b85ab2f42
ObjectOneBB <cornergif@yandex.ru>
2016-05-15 16:49:52 +0300

    first commit

---
+++ src/valueTypes/valueTypes1.java
@@ -0,0 +1,35 @@
+package valueTypes;
+
+import static java.lang.System.out;
+
+public class valueTypes1 {
+	public static void main(String[] args) {
+		short a = 12121;
+		System.out.println("hello world!zvm_14_05_2016 = " + a);
+		
+		double dbl = 23.5;
+		System.out.println("dbl = " + dbl);
+		
+		float flt = 15;
+		System.out.println("flt = " + flt);
+		
+		byte bt1= Byte.MAX_VALUE;
+		System.out.println("bt1 = " + bt1);
+		
+		short sh1= Short.MAX_VALUE;
+		System.out.println("short sh1= Short.MAX_VALUE = " + sh1);
+		
+		int i1nt= Integer.MAX_VALUE;
+		System.out.println("int i1nt= Integer.MAX_VALUE = " + i1nt);
+		
+		long lng = Long.MAX_VALUE;
+		System.out.println("long lng = Long.MAX_VALUE = " + lng);
+		
+		long kvntln = 1000000000;
+		System.out.println("kvntln = " + kvntln);
+
+		char ch = 'A';
+		out.println("char ch = 'A' = " + ch);
+
+	}
+}
